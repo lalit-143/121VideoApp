77824fe5434616b021d1e0cb8a47f6357b279661
lalit-143 <82702053+lalit-143@users.noreply.github.com>
2024-09-25 02:08:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,23 @@
 const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 8080 });
 
-const sendPORT = 8080;
-const receivePORT = 8081;
-
-const sendServer = new WebSocket.Server({ port: sendPORT });
-const receiveServer = new WebSocket.Server({ port: receivePORT });
-
-sendServer.on('connection', socket => {
-    console.log('Send server: Client connected');
+server.on('connection', socket => {
+    console.log('Client connected');
     
     socket.on('message', message => {
-        console.log('Send server: Data received from client');
-        // Broadcast the received message to all connected clients on the receive server
-        receiveServer.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
+        console.log('Data received from client');
+        // Broadcast the received message to all connected clients
+        server.clients.forEach(client => {
+            if (client !== socket && client.readyState === WebSocket.OPEN) {
                 client.send(message);
-                console.log('Send server: Data sent to receiver');
+                console.log('Data sent to client');
             }
         });
     });
 
     socket.on('close', () => {
-        console.log('Send server: Client disconnected');
+        console.log('Client disconnected');
     });
 });
 
-receiveServer.on('connection', socket => {
-    console.log('Receive server: Client connected');
-    
-    socket.on('message', message => {
-        console.log('Receive server: Data received from client');
-        // Broadcast to send server clients (if needed)
-    });
-
-    socket.on('close', () => {
-        console.log('Receive server: Client disconnected');
-    });
-});
-
-console.log('WebSocket servers are running');
+console.log('WebSocket server is running on ws://localhost:8080');
