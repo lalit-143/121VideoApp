da48ffe2f7c1ef8c83402e498006ae11ba948456
lalit-143 <82702053+lalit-143@users.noreply.github.com>
2024-09-25 01:45:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const WebSocket = require('ws');
-const sendServer = new WebSocket.Server({ port: 8080 });
-const receiveServer = new WebSocket.Server({ port: 8081 });
+
+const sendPORT = process.env.PORT || 8080;
+const receivePORT = process.env.PORT || 8081;
+
+const sendServer = new WebSocket.Server({ port: sendPORT });
+const receiveServer = new WebSocket.Server({ port: receivePORT });
 
 sendServer.on('connection', socket => {
     console.log('Send server: Client connected');
@@ -34,4 +38,4 @@ receiveServer.on('connection', socket => {
     });
 });
 
-console.log('WebSocket servers are running on ws://localhost:8080 (send) and ws://localhost:8081 (receive)');
+console.log('WebSocket servers are running');
